176640e49bba13c91f8e6e42d5613c41ec8c3c3d
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-06-03 15:37:00 +0200

    Fixed path issues

--- euler/py_scripts/common/helper.py
+++ euler/py_scripts/common/helper.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
@@ -5,7 +7,15 @@ from sqlalchemy.engine import URL
 from euler.py_scripts.common import conf
 
 
-def write_df(df: pd.DataFrame, table, truncate=True):
+def get_project_path():
+    helper_path = os.path.abspath(__file__)
+    common_path = os.path.dirname(helper_path)
+    py_scripts_path = os.path.dirname(common_path)
+    euler_path = os.path.dirname(py_scripts_path)
+    return euler_path
+
+
+def get_url():
     url = URL.create(
         drivername='redshift+redshift_connector',
         host=conf.REDSHIFT_URL,
@@ -14,7 +24,15 @@ def write_df(df: pd.DataFrame, table, truncate=True):
         username=conf.REDSHIFT_LOGIN,
         password=conf.REDSHIFT_PASSWORD
     )
-    with create_engine(url=url).connect() as conn:
+    return url
+
+
+def get_connection():
+    return create_engine(url=get_url()).connect()
+
+
+def write_df(df: pd.DataFrame, table, truncate=True):
+    with get_connection() as conn:
         if truncate:
             conn.execute(f"truncate table {conf.SCHEMA}.{table};")
         df.to_sql(
@@ -24,4 +42,3 @@ def write_df(df: pd.DataFrame, table, truncate=True):
             index=False,
             chunksize=1000,
             if_exists='append')
-
--- euler/py_scripts/seed_data.py
+++ euler/py_scripts/seed_data.py
@@ -1,16 +1,17 @@
+import os.path
 from datetime import datetime, tzinfo, timezone
 
 from euler.py_scripts.common import conf
 import pandas as pd
 
-from euler.py_scripts.common.helper import write_df
+from euler.py_scripts.common.helper import write_df, get_project_path
 
 
 def main():
     tables = [*conf.ADS_LOCAL_TABLES.values(), conf.PRODUCT_MASTER_TABLE]
-
+    project_root_path = get_project_path()
     for table in tables:
-        path = f"euler/data/{table}.csv"
+        path = os.path.join(project_root_path, f"data/{table}.csv")
         df = pd.read_csv(path)
         if "updated_date" in df.columns:
             df["updated_date"] = datetime.now(tz=timezone.utc)
