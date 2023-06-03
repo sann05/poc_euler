118029d23f08e7606eec501521ef6bbc3bacebb7
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-06-03 16:42:12 +0200

    Added run_daily script

---
+++ euler/py_scripts/run_daily.py
@@ -0,0 +1,35 @@
+import os
+
+from euler.py_scripts.common import conf
+from euler.py_scripts.common.helper import get_project_path, get_connection
+
+
+def main():
+    project_root_path = get_project_path()
+    scripts_dir_path = 'sql_queries/scheduled/'
+    for locality, table_name in conf.ADS_LOCAL_TABLES.items():
+        with get_connection() as conn:
+
+            with conn.connection.cursor() as cur:
+                conn.connection.autocommit = False
+                for script in [
+                    # "delete_rows.sql",
+                    # "update_rows.sql",
+                    "insert_rows.sql"
+                ]:
+                    script_path = os.path.join(
+                        project_root_path,
+                        scripts_dir_path,
+                        script
+                    )
+                    with open(script_path, 'r') as f:
+                        script_parsed = f.read().format(locality=locality)
+                        script_array = [s.strip() for s in script_parsed.split(';') if s.strip()]
+                        print(f"Executing script {script} on table {table_name}")
+                        for st in script_array:
+                            cur.execute(st)
+                        conn.connection.commit()
+
+
+if __name__ == '__main__':
+    main()
