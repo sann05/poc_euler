ffc6329e85c55e03af50e56afb8013e8181757f6
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-06-04 11:15:03 +0200

    Added using identity in seed_data script

--- euler/py_scripts/seed_data.py
+++ euler/py_scripts/seed_data.py
@@ -1,20 +1,24 @@
 import os.path
-from datetime import datetime, tzinfo, timezone
+from datetime import datetime, timezone
 
-from euler.py_scripts.common import conf
 import pandas as pd
 
-from euler.py_scripts.common.helper import write_df, get_project_path
+from euler.py_scripts.common import conf
+from euler.py_scripts.common.helper import write_df, get_project_path, \
+    recreate_tables
 
 
 def main():
-    tables = [*conf.ADS_LOCAL_TABLES.values(), conf.PRODUCT_MASTER_TABLE]
+    tables = [ conf.PRODUCT_MASTER_TABLE, *conf.ADS_LOCAL_TABLES.values()]
     project_root_path = get_project_path()
+    recreate_tables()
     for table in tables:
         path = os.path.join(project_root_path, f"data/{table}.csv")
         df = pd.read_csv(path)
         if "updated_date" in df.columns:
             df["updated_date"] = datetime.now(tz=timezone.utc)
+        if "id" in df.columns:
+            df.drop("id", axis=1, inplace=True)
         print(f"Seeding {table}")
         write_df(df, table)
 
