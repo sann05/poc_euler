77e92019f04134a1976c8751384133d7f9f52b92
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-06-04 17:48:58 +0200

    Added mixed_scenario

---
+++ euler/sql_queries/tests/5_scenario_mixed.sql
@@ -0,0 +1,19 @@
+insert into product_master(product)
+values
+('VR_Game'),
+('VR_Glasses');
+
+insert into ads_us(date, products)
+values
+('2022-10-14','VR_Game, VR_Glasses');
+
+insert into ads_uk(date, products)
+values
+('2022-10-14','VR_Game, VR_Glasses'),
+('2022-10-14','VR_Glasses');
+
+
+insert into ads_online(date, products)
+values
+('2022-10-14','VR_Game, VR_Glasses'),
+('2022-10-14','VR_Game');
